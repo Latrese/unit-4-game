28a3246c330879767cf0efa2c284b1631f10478b
Latrese <latrese.flowers1979@gmail.com>
2019-05-31 12:22:26 -0700

    inprogress

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+var winCount = 0;
+var lossCount = 0;
+var currentScore = 0;
+var targetScore = 0;
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function startGame(){
+    currentScore = 0;
+    targetScore = getRandomArbitrary(19, 120)
+
+    $("#crystalOne").data("value", getRandomArbitrary(1, 12));
+    $("#crystalTwo").data("value", getRandomArbitrary(1, 12));
+    $("#crystalThree").data("value", getRandomArbitrary(1, 12));
+    $("#crystalFour").data("value", getRandomArbitrary(1, 12));
+    $("#targetScore").text(targetScore)
+    $("#userScore").text(currentScore)
+    
+}
+startGame();
+$("#crystalOne").on('click', function() {
+    console.log($(this).data("value"))
+ });
+ $("#crystalTwo").on('click', function() {
+    currentScore = currentScore += $(this).data('value');
+    $('#userScore').text(currentScore)
+ });
