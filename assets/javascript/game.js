c5df16b7e5c460054b90aa706f16aa504e502da5
Latrese <latrese.flowers1979@gmail.com>
2019-06-01 19:17:21 -0700

    stillworking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,9 +20,26 @@ function startGame(){
 }
 startGame();
 $("#crystalOne").on('click', function() {
-    console.log($(this).data("value"))
+    currentScore = currentScore += $(this).data('value');
+    $('#userScore').text(currentScore)
  });
+
  $("#crystalTwo").on('click', function() {
     currentScore = currentScore += $(this).data('value');
     $('#userScore').text(currentScore)
  });
+
+ $("#crystalThree").on('click', function() {
+    currentScore = currentScore += $(this).data('value');
+    $('#userScore').text(currentScore)
+ });
+
+ $("#crystalFour").on('click', function() {
+    currentScore = currentScore += $(this).data('value');
+    $('#userScore').text(currentScore)
+ });
+
+ if (currentScore === targetScore) {
+   wins++;
+
+ }
